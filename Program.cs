68d2bafa9089de1862fa010aafefc3d8ae5db045
Acer-User <Acer-User@DESKTOP-NSD4PLV>
2024-01-22 12:50:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_2_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("       ##");
+            Console.WriteLine("     #  #");
+            Console.WriteLine("   #    #");
+            Console.WriteLine("  #     #");
+            Console.WriteLine(" ########");
+
+            //to pause the console else it closes on its own!
+            Console.ReadLine();
+
+        }
+    }
+}
